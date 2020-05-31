9886cd70c605b1a3e6e5ba7cc104af358cee280b
Loner <Kaerkillheart@sjtu.edu.cn>
2020-05-31 08:58:42 +0800

    update midterm inspection

--- game2048/agents.py
+++ game2048/agents.py
@@ -1,4 +1,5 @@
 import numpy as np
+import copy as cp
 
 class Agent:
     '''Agent Base.'''
@@ -24,19 +25,56 @@ class Agent:
         direction = int(input("0: left, 1: down, 2: right, 3: up = ")) % 4
         return direction
 
-class MyAgent(Agent):
-
-    def __init__(self, game, display=None, modelpath):
-        if game.size != 4:
-            raise ValueError(
-                "`%s` can only work with game of `size` 4." % self.__class__.__name__)
-        super().__init__(game, display)
-        import torch
-        self.model = torch.load(modelpath)
+class SerialAgent(Agent):
     
+    def __init__(self, game, display=None):
+        self.game = game
+        self.display = display
+        self.cnt = 0
+
     def step(self):
-        direction = self.model(self.game.board)
+        self.cnt += 1
+        return self.cnt % 4
+
+class ReinforcedAgent(Agent):
+    
+    def __init__(self, game, display=None):
+        super().__init__(game, display)
+        self.tmpmax = 0
+
+    def __iterstep(self, game, steps=1):
+        if steps == 1:
+            for i in range(4):
+                tmpgame = cp.deepcopy(game)
+                tmpgame.move(i)
+                score = tmpgame.score
+                if score > self.tmpmax:
+                    self.tmpmax = score
+                    return 1
+                
+        else:
+            tmpr = 0
+            for i in range(4):
+                tmpgame = cp.deepcopy(game)
+                tmpgame.move(i)
+
+    def step(self):
+        max = 0
+        for i in range(4):
+            tmpgame1 = cp.deepcopy(self.game)
+            tmpgame1.move(i)
+            for j in range(4):
+                tmpgame2 = cp.deepcopy(tmpgame1)
+                tmpgame2.move(i)
+                for k in range(4):
+                    tmpgame3 = cp.deepcopy(tmpgame2)
+                    tmpgame3.move(i)
+                    tmpscore = tmpgame3.score
+                    if tmpscore > max:
+                        direction = i
+                        max = tmpscore            
         return direction
+
 
 
 class RandomAgent(Agent):
